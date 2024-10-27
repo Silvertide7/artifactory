ef365e9374a16059abb1fdf47c8cce887cbfa9c7
Silvertide7 <natephillips801@gmail.com>
2024-10-27 01:19:09 -0600

    Allow block to be placed directionally. Make it so it can only be mined by an iron axe or above.

--- src/main/java/net/silvertide/artifactory/blocks/AttunementNexusBlock.java
+++ src/main/java/net/silvertide/artifactory/blocks/AttunementNexusBlock.java
@@ -1,6 +1,7 @@
 package net.silvertide.artifactory.blocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -8,33 +9,36 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.silvertide.artifactory.gui.AttunementNexusAttuneMenu;
-import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 
 public class AttunementNexusBlock extends Block {
-    public static final VoxelShape SHAPE = Block.box(0,0,0,16,15,16);
+    public static final VoxelShape SHAPE = Block.box(1, 0, 3, 14, 14, 13);
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    private static final Component CONTAINER_TITLE = Component.translatable("container.artifactory.attunement_nexus");
 
     public AttunementNexusBlock(Properties properties) {
-        super(BlockBehaviour.Properties.of().strength(5.0F).sound(SoundType.ANCIENT_DEBRIS).noOcclusion().lightLevel(value -> 14));
+        super(Properties.copy(Blocks.ENCHANTING_TABLE).noOcclusion().sound(SoundType.WOOD));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Nullable
     @SuppressWarnings("deprecation")
     public MenuProvider getMenuProvider(BlockState state, Level level, BlockPos pos) {
         return new SimpleMenuProvider((i, inventory, player) ->
-                new AttunementNexusAttuneMenu(i, inventory, ContainerLevelAccess.create(level, pos)), Component.translatable("block.artifactory.attunement_nexus"));
+                new AttunementNexusAttuneMenu(i, inventory, ContainerLevelAccess.create(level, pos)), CONTAINER_TITLE);
     }
 
     @SuppressWarnings("deprecation")
@@ -46,18 +50,34 @@ public class AttunementNexusBlock extends Block {
             return InteractionResult.CONSUME;
         }
     }
-
     @Override
     @SuppressWarnings("deprecation")
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
     }
 
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
     @Override
     @SuppressWarnings("deprecation")
-    public @NotNull RenderShape getRenderShape(BlockState state) {
-        return RenderShape.MODEL;
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState blockState) {
+        return RenderShape.MODEL;
+    }
 }
