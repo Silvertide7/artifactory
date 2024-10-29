a2015b00f3d575b8715c9ed763b8cc522cf6a03a
Silvertide7 <natephillips801@gmail.com>
2024-10-29 15:46:49 -0600

    Update voxelshape and account for different axis

--- src/main/java/net/silvertide/artifactory/blocks/AttunementNexusBlock.java
+++ src/main/java/net/silvertide/artifactory/blocks/AttunementNexusBlock.java
@@ -15,17 +15,23 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.silvertide.artifactory.gui.AttunementNexusAttuneMenu;
 
 import javax.annotation.Nullable;
 
 public class AttunementNexusBlock extends Block {
-    public static final VoxelShape SHAPE = Block.box(1, 0, 3, 14, 14, 13);
+    public static final VoxelShape BASE = Block.box(3.0D, 0D, 3.0D, 13.0D, 1.0D, 13.0D);
+    public static final VoxelShape MIDDLE_X_AXIS = Block.box(5.0D, 1.0D, 4.0D, 11.0D, 10.0D, 12.0D);
+    public static final VoxelShape MIDDLE_Z_AXIS = Block.box(4.0D, 1.0D, 5.0D, 12.0D, 10.0D, 11.0D);
+    public static final VoxelShape ANVIL_X_AXIS= Block.box(5.0D, 10.0D, 1.0D, 11.0D, 14.0D, 15.0D);
+    public static final VoxelShape ANVIL_Z_AXIS= Block.box(1.0D, 10.0D, 5.0D, 15.0D, 14.0D, 11.0D);
+    public static final VoxelShape SHAPE_X_AXIS = Shapes.or(BASE, MIDDLE_X_AXIS, ANVIL_X_AXIS);
+    public static final VoxelShape SHAPE_Z_AXIS = Shapes.or(BASE, MIDDLE_Z_AXIS, ANVIL_Z_AXIS);
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     private static final Component CONTAINER_TITLE = Component.translatable("container.artifactory.attunement_nexus");
 
@@ -53,7 +59,8 @@ public class AttunementNexusBlock extends Block {
     @Override
     @SuppressWarnings("deprecation")
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPE;
+        Direction direction = state.getValue(FACING);
+        return direction.getAxis() == Direction.Axis.X ? SHAPE_X_AXIS : SHAPE_Z_AXIS;
     }
 
     public BlockState getStateForPlacement(BlockPlaceContext context) {
