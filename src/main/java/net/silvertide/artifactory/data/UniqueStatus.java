6c7edea4c7dec0c0332db1ee3dcc88d0f796890e
Silvertide7 <natephillips801@gmail.com>
2025-03-28 22:19:59 -0600

    Fixes to json data, remove some unique modifier code.

--- src/main/java/net/silvertide/artifactory/data/UniqueStatus.java
+++
@@ -1,7 +0,0 @@
-package net.silvertide.artifactory.data;
-
-public class UniqueStatus {
-    public static final String ALREADY_ATTUNED_BY_THIS_PLAYER = "ALREADY_ATTUNED_BY_THIS_PLAYER";
-    public static final String ATTUNED_BY_ANOTHER_PLAYER = "ATTUNED_BY_ANOTHER_PLAYER";
-    public static final String REACHED_UNIQUE_CAPACITY = "REACHED_UNIQUE_CAPACITY";
-}
